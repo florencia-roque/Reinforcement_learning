49338331e40952bbf25db815c4c0065c0ef6daf4
Florencia Roque <froque@ute.com.uy>
2025-08-22 15:57:39 -0300

    limpiando repo para la entrega

--- Proyecto_Final/pseudo.py
+++
@@ -1,6 +0,0 @@
-for i = 0 to escenarios:
-    h=inicializarHidrologia(hidroIni)
-    escenario[i] = nuevoEscenario
-    for j = 0 to 51:
-        agregarSemana(escenario[i],aportes(h,j))
-        h=siguienteHidrologia
